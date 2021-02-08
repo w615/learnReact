c8038b7e82b58bce49094d93ff7f06ec1e5a5334
wdc <1812991192@qq.com>
2021-02-08 23:48:01 +0800

    css in js

--- src/clsaaStyle.js
+++ src/clsaaStyle.js
@@ -1,15 +1,29 @@
-import React,{Component} from 'react'
-import './style.css'
+import React,{Component} from 'react';
+import classNames from 'classnames/bind';
+import DivContainer  from  './styleCss.js';
+import styles from './style.css';
+
+let cx = classNames.bind(styles);
 // 动态添加css
-const styles = {
-  color:'red',
-  fontSize:'100px'
-}
+// const styles = {
+//   color:'red',
+//   fontSize:'100px'
+// }
 class ClassStyle extends Component {
   render () {
+    // js逻辑
+    let className = cx({
+      font:true,
+    })
     return (
       // <div style = { {fontSize: '50px'} ,styles}>王</div>
-      <div className={'font'}>王</div>
+      // <div className={'font'}>王</div>
+      <>
+       <div className={className}>王</div>
+       <DivContainer>东</DivContainer>
+      
+        <div>东</div>
+        </>
     )
   }
 }
---
+++ src/styleCss.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const DivContainer = styled.div`
+font-size:200px;
+color:red;
+`
+export default DivContainer
